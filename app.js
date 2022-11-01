8427336c2b6ed5a9ff82c3d67712841992bbe1d5
Pablo <pabolo333@gmail.com>
2022-11-01 20:02:37 +0100

    flex cheatsheet and spacing

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import japAlpha from './japAlpha.json' assert {type: 'json'};
 
 const input = document.getElementById("input");
 const text = document.getElementById("text");
-let current = japAlpha.hiragana;
-
+const cheatsheet = document.getElementById("cheatsheet_text");
 const options = document.querySelectorAll("input[name='jap_option']")
+let current = japAlpha.hiragana;
+changeCheatsheet(japAlpha.hiragana);
+
 console.log(options);
 
 for (const option of options) {
@@ -18,6 +20,9 @@ for (const option of options) {
     });
 }
 
+
+
+
 input.addEventListener("keyup", () => {
     translate();
 })
@@ -26,7 +31,7 @@ function translate(){
     let upper = input.value.toUpperCase();
     text.innerText = ""
     for(let i = 0; upper.length > i; i+=2){
-        
+        let sub2 = upper.substring(i,i+2)
         if(upper[i] == "A" || upper[i] == "I" || upper[i] == "U" || upper[i] == "E" || upper[i] == "O" || upper[i] == "N" || upper[i] == " "){
             if (upper[i] == " ") {
                 text.innerHTML += "&nbsp"
@@ -34,10 +39,20 @@ function translate(){
                 text.innerText += current[upper[i]] != undefined ? current[upper[i]] : ""  ;
             }
             i--;
-        } else{
-            text.innerText += current[upper.substring(i,i+2)] != undefined ? current[upper.substring(i,i+2)] : "" ;
+        } 
+        else{
+            text.innerText += current[sub2] != undefined ? current[sub2] : "" ;
         }
     }  
 }
 
+function changeCheatsheet(alpha){
+    cheatsheet.innerHTML = "";  
 
+    for (const i in alpha) {
+        let div = document.createElement("div");
+        div.innerHTML =`${i} = ${alpha[i]}`;
+        cheatsheet.appendChild(div)
+
+    }
+}
